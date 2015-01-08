1244c2e575d83518a2638a2d497acd43cf90001f
dicm.dk <dicm.dk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 09:41:28 +0000

    Release
    git-svn-id: https://plugins.svn.wordpress.org/dicmdk-change-username/trunk@1062922 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dicmdk-changeusername.php
@@ -0,0 +1,153 @@
+<?php
+/*
+Plugin Name: Dicm.dk - Change username
+Plugin URI: http://dicm.dk
+Description: Allows site admin's to change username of any user wihtout using any other tools. Plugin requires Dicm.dk - Toolbox to work
+Version: 1.0
+Author: Kim Vinberg - dicm.dk
+Author URI: http://dicm.dk
+License: 
+
+Free for personal use
+
+*/
+/*
+register_activation_hook( __FILE__, 'child_plugin_activate' );
+function child_plugin_activate(){
+
+    // Require parent plugin
+    if ( ! is_plugin_active( 'dicmdk-toolbox/dicmdk-toolbox.php' ) and current_user_can( 'activate_plugins' ) ) {
+        // Stop activation redirect and show error
+        wp_die('Sorry, but this plugin requires the Parent Plugin dicmdk-toolbox to be installed and active. <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Return to Plugins</a>');
+    }
+}
+
+*/
+
+
+add_action('admin_menu', 'my_plugin_menu');
+add_action( 'admin_init', 'dicmdktoolbox_change_username_settings_init' );
+
+
+if(!function_exists('dicmdktoolbox_options_page')) {
+function dicmdktoolbox_options_page(  ) { 
+
+	?>
+	<form action='options.php' method='post'>
+		
+		<h2>Dicm.dk - Toolbox</h2>
+		
+		<?php
+		settings_fields( 'pluginPage' );
+		do_settings_sections( 'pluginPage' );
+		submit_button();
+		?>
+		
+	</form>
+	<?php
+
+}
+}
+
+function my_plugin_menu() {
+add_menu_page( 'Dicm.dk - Toolbox', 'Dicm.dk - Toolbox', 'manage_options', 'dicmdktoolbox', 'dicmdktoolbox_options_page', '', 0 );
+//add_submenu_page( 'dicmdktoolbox', 'Change username', 'Change username', 'manage_options', 'my-secondary-slug', 'dicmdktoolbox_change_username_options_page');
+}
+
+
+function dicmdktoolbox_change_username_settings_init(  ) { 
+
+  if ( $_GET['settings-updated'] == 'true' && is_admin()) {
+   	
+	$options = get_option( 'dicmdktoolbox_settings' );
+
+	$old_username = sanitize_text_field($options['dicmdktoolbox_change_username_text_field_0']);
+	$new_username = sanitize_text_field($options['dicmdktoolbox_change_username_text_field_1']);
+	
+	 $old_user = get_user_by( 'login', $old_username );
+	 $new_user = get_user_by( 'login', $new_username );
+	 
+	 if($old_user->ID != '' && $new_user->ID == '' && $new_username != '' && $old_username != '') { //If old user esists and no user with new name esists.
+
+		global $wpdb;
+		$wpdb->query( "UPDATE $wpdb->users SET user_login = '".$new_username."' WHERE user_login='".$old_username."';" );
+	
+	 }
+
+	}
+		
+	register_setting( 'pluginPage', 'dicmdktoolbox_settings' );	
+
+add_settings_section(
+		'dicmdktoolbox_change_username_pluginPage_section', 
+		__( 'Change username', 'dicmdktoolbox' ), 
+		'dicmdktoolbox_change_username_settings_section_callback', 
+		'pluginPage'
+	);
+
+	add_settings_field( 
+		'dicmdktoolbox_text_field_0', 
+		__( 'Old username', 'dicmdktoolbox' ), 
+		'dicmdktoolbox_change_username_text_field_0_render', 
+		'pluginPage', 
+		'dicmdktoolbox_change_username_pluginPage_section' 
+	);	
+	add_settings_field( 
+		'dicmdktoolbox_text_field_1', 
+		__( 'New username', 'dicmdktoolbox' ), 
+		'dicmdktoolbox_change_username_text_field_1_render', 
+		'pluginPage', 
+		'dicmdktoolbox_change_username_pluginPage_section' 
+	);
+
+	add_settings_field( 
+		'dicmdktoolbox_text_field_submit', 
+		'', 
+		'dicmdktoolbox_change_username_text_field_submit_render', 
+		'pluginPage', 
+		'dicmdktoolbox_change_username_pluginPage_section' 
+	);
+
+
+}
+
+
+function dicmdktoolbox_change_username_text_field_0_render(  ) { 
+
+	$options = get_option( 'dicmdktoolbox_change_username__settings' );
+	?>
+	<input type='text' name='dicmdktoolbox_settings[dicmdktoolbox_change_username_text_field_0]' value='<?php if($options['dicmdktoolbox_change_username_text_field_0'] == '' ) { echo "admin"; } else { echo $options['dicmdktoolbox_change_username_text_field_0']; } ?>'>
+	<?php
+
+}
+
+function dicmdktoolbox_change_username_text_field_1_render(  ) { 
+
+	$options = get_option( 'dicmdktoolbox_change_username__settings' );
+	?>
+	<input type='text' name='dicmdktoolbox_settings[dicmdktoolbox_change_username_text_field_1]' value='<?php echo $options['dicmdktoolbox_change_username_text_field_0']; ?>'>
+	<?php
+
+}
+
+function dicmdktoolbox_change_username_text_field_submit_render(  ) { 
+
+	//$options = get_option( 'dicmdktoolbox_change_username__settings' );
+	submit_button();
+
+}
+
+
+
+function dicmdktoolbox_change_username_settings_section_callback(  ) { 
+
+	echo __('Allows site administrators to change a username easily in 3 steps.<br>
+		1: Enter Old username (example: admin)<br>
+		2: Enter new username (Example: administrator)<br>
+		3: Press "Save Changes".<br>
+		The username is now changed.<br><br> <font color="red">IMPORTANT: If the change fail, you will no longer be able to login before manually changing the username (create a backup user before changing)</font>.', 'dicmdktoolbox' );
+
+}
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Dicm.dk - Change username ===
+Contributors: dicm.dk
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CWEYAQKSLPGWL
+Tags: toolbox, download plugin, plugin stealer
+Requires at least: 4.1
+Tested up to: 4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Sub plugin for dicm.dk - Toolbox
+
+== Description ==
+
+Changing usernames in Wordpress is currently annoying, and without external software it is really a pain. With this plugin you can enter the username you want to change and enter the new username. Then submit.
+
+The username will now be changed and you can use it to login with.
+
+Created to help people change from "Admin" username to something else, without having to use phpmyadmin or similer tools. 
+
+Does not work on multisites (ask for update if needed).
+
+
+== Installation ==
+
+1. Upload `dicmdk-changeusername.php`to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. If you do not have dicm.dk - Toolbox, install it. (https://wordpress.org/plugins/dicmdk-toolbox/)
+
+== Frequently asked questions ==
+
+== Screenshots ==
+
+
+== Changelog ==
+1.0.0 
+ - First version of plugin released
+
+== Upgrade notice ==
+
+Upgrade using the wordpress update button.
+
+== Arbitrary section 1 ==
+
